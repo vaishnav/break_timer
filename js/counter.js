bfe21b0c1acd083961a0dc6291ad598921ebfbed
vaishnav <vaishnav291999@gmail.com>
2020-06-09 18:01:12 +0530

    fixed bugs

--- js/counter.js
+++ js/counter.js
@@ -1,28 +1,48 @@
 "use strict";
 
-var final_hour, final_minute, final_seconds;
-var newDateObj = new Date((new Date()).getTime() + 45*60000);
+var final_hour, final_minute, final_seconds,newDateObj,t;
+
 
 function counter() {
+    newDateObj = new Date((new Date()).getTime() + 45*60000);
+    // newDateObj = new Date((new Date()).getTime() + 10000);        //For testing  
+    cnt();
+}
+
+function cnt() {
 
     var date = new Date()
     var diff = newDateObj - date;
 
+    var minute;
+    var second;
+
     var min = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
     var sec = Math.floor((diff % (1000 * 60)) / 1000);
 
-    if(min <= 9){
-        min = '0' + min;
+    minute = min;
+    second = sec;
+
+    if(minute <= 9){
+        minute = '0' + minute;
     }
 
-    if(sec <= 9){
-        sec = '0' + sec;
+    if(second <= 9){
+        second = '0' + second;
     }
 
-    var clock = min + ":" + sec;
+    var clock = minute + ":" + second;
     document.getElementById("count").innerHTML = clock
 
-    setTimeout(counter,1000);
+
+    t = setTimeout(cnt,1000);
+
+    if(min <= 0){
+        if(sec <= 0){
+            clearTimeout(t);
+            document.getElementById("count").innerHTML = "Start Again";
+        }
+    }
 }
 
 // counter();
